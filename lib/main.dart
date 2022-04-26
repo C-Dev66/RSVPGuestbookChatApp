136aff59d4f5d09260263e71dda9214ea450f7b9
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-04-26 06:30:05 -0700

    update HomePage
    Correctly construct GuestBook with new messages parameter

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,9 @@ class HomePage extends StatelessWidget {
                   GuestBook(
                       addMessage: (message) => appState.addMessageToGuestBook(
                             message,
-                          )),
+                          ),
+                        messages: appState.GuestBookMessages,
+                  ),
                 ],
               ],
             ),
@@ -130,7 +132,8 @@ class ApplicationState extends ChangeNotifier {
         });
       } else {
         _loginState = ApplicationLoginState.loggedOut;
-
+        _guestBookMessages = [];
+        _guestBookSubscription?.cancel();
       }
       notifyListeners();
     });
@@ -231,8 +234,9 @@ class GuestBookMessage {
 }
 
 class GuestBook extends StatefulWidget {
-  const GuestBook({required this.addMessage});
+  const GuestBook({required this.addMessage, required this.messages});
   final FutureOr<void> Function(String message) addMessage;
+  final List<GuestBookMessage> messages;
 
   @override
   _GuestBookState createState() => _GuestBookState();
@@ -244,42 +248,52 @@ class _GuestBookState extends State<GuestBook> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Form(
-        key: _formkey,
-        child: Row(
-          children: [
-            Expanded(
-                child: TextFormField(
-              controller: _controller,
-              decoration: const InputDecoration(hintText: 'Leave a message'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Enter your message to continue';
-                }
-                return null;
-              },
-            )),
-            const SizedBox(width: 8),
-            StyledButton(
-              onPressed: () async {
-                if (_formkey.currentState!.validate()) {
-                  await widget.addMessage(_controller.text);
-                  _controller.clear();
-                }
-              },
-              child: Row(
-                children: const [
-                  Icon(Icons.send),
-                  SizedBox(width: 4),
-                  Text('SEND'),
-                ],
-              ),
-            )
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formkey,
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                  controller: _controller,
+                  decoration:
+                      const InputDecoration(hintText: 'Leave a message'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your message to continue';
+                    }
+                    return null;
+                  },
+                )),
+                const SizedBox(width: 8),
+                StyledButton(
+                  onPressed: () async {
+                    if (_formkey.currentState!.validate()) {
+                      await widget.addMessage(_controller.text);
+                      _controller.clear();
+                    }
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(Icons.send),
+                      SizedBox(width: 4),
+                      Text('SEND'),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+        const SizedBox(height: 8),
+        for (var message in widget.messages)
+          Paragraph('${message.name}: ${message.message}'),
+        const SizedBox(height: 8),
+      ],
     );
   }
 }
